f7730fa8624871d94e8e3c0ce128e59a1d537cfc
sagbeyeg <agbeyegbesisan@gmail.com>
2021-03-16 18:30:50 -0400

    attempting to add bootstrap to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import AboutMe from './Components/AboutMe';
+// import {
+//   Menu
+// } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to Sisan's Portfolio</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = { 
+    activeItem: 'home' 
+  }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+  render() {
+
+    return (
+      <div className="App">
+        {/* <Menu pointing secondary>
+          <Menu.Item
+            name='home'
+            active={activeItem === 'home'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='messages'
+            active={activeItem === 'messages'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Item
+            name='friends'
+            active={activeItem === 'friends'}
+            onClick={this.handleItemClick}
+          />
+          <Menu.Menu position='right'>
+            <Menu.Item
+              name='logout'
+              active={activeItem === 'logout'}
+              onClick={this.handleItemClick}
+            />
+          </Menu.Menu>
+        </Menu> */}
+        <h1>Welcome to Sisan's Portfolio</h1>
+        <AboutMe/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutMe(props) {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <div>Hi! I'm Sisan, a software engineer based in the New York City area.</div>
+    </div>
+  );
+}
+
+export default AboutMe;
