23cb21712d3d4aaa30aba0a55ac5eaa4ca2cdfac
sagbeyeg <agbeyegbesisan@gmail.com>
2021-03-17 14:38:29 -0400

    added bootstrap and semantic ui + working on navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './App.css';
 import AboutMe from './Components/AboutMe';
-// import {
-//   Menu
-// } from "semantic-ui-react";
+// import 'semantic-ui-css/semantic.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Menu
+} from "semantic-ui-react";
 
 class App extends React.Component {
   state = { 
@@ -16,30 +18,30 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        {/* <Menu pointing secondary>
+        <Menu pointing secondary>
+          <Menu.Menu position='right'>
           <Menu.Item
             name='home'
-            active={activeItem === 'home'}
+            active={this.activeItem === 'home'}
             onClick={this.handleItemClick}
           />
           <Menu.Item
             name='messages'
-            active={activeItem === 'messages'}
+            active={this.activeItem === 'messages'}
             onClick={this.handleItemClick}
           />
           <Menu.Item
             name='friends'
-            active={activeItem === 'friends'}
+            active={this.activeItem === 'friends'}
             onClick={this.handleItemClick}
           />
-          <Menu.Menu position='right'>
             <Menu.Item
               name='logout'
-              active={activeItem === 'logout'}
+              active={this.activeItem === 'logout'}
               onClick={this.handleItemClick}
             />
           </Menu.Menu>
-        </Menu> */}
+        </Menu>
         <h1>Welcome to Sisan's Portfolio</h1>
         <AboutMe/>
       </div>
