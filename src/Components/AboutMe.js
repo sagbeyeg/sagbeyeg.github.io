cbd32ea9485ce8b2b2b909c065e33bc5dd79d100
sagbeyeg <agbeyegbesisan@gmail.com>
2021-03-20 14:59:51 -0400

    About me draft

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -4,7 +4,7 @@ function AboutMe(props) {
   return (
     <div>
       <h2>About Me</h2>
-      <div>Hi! I'm Sisan, a software engineer based in the New York City area.</div>
+      <div>Hi! I'm Sisan, a Software Engineer based in the New York City area and a Flatiron School graduate👩🏾‍🎓. Prior to that, I worked for 2 years in Information Technology as an IT Support Specialist and while at Wellesley College I organized various event around diversity and inclusion. Moving forward, I want to take my passion for community, my technical skills, and my ability to work well under pressure to provide solutions for pur community and make technology more inclusive.</div>
     </div>
   );
 }
